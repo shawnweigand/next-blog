987cd57b37ba42d00c3d581b751eab726eb996b0
Weigand, Shawn <sweigand@estee.com>
2024-09-06 16:00:41 -0400

    separate form

---
+++ src/app/partials/form.tsx
@@ -0,0 +1,27 @@
+
+export default function Form() {
+    return (
+        <form className="grid">
+        {/* Title */}
+        <h2 className="text-black">Title</h2>
+        <input className="w-full p-4 mt-4 mb-10 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-lg focus:ring-indigo-500 focus:border-indigo-500" placeholder="Write your blog title here..." />
+        {/* Content */}
+        <h2 className="text-black">Content</h2>
+        <textarea className="w-full h-32 max-h-64 p-4 mt-4 mb-10 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-lg focus:ring-indigo-500 focus:border-indigo-500" placeholder="Write your blog post here..." />
+        {/* Author */}
+        <div className="grid gap-4 grid-cols-2">
+            <div className="flex flex-col justify-start">
+            <h2 className="text-black">First Name</h2>
+            <input className="w-full p-4 mt-4 mb-10 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-lg focus:ring-indigo-500 focus:border-indigo-500" placeholder="Your first name..." />
+            </div>
+            <div className="flex flex-col justify-start">
+            <h2 className="text-black">Last Name</h2>
+            <input className="w-full p-4 mt-4 mb-10 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-lg focus:ring-indigo-500 focus:border-indigo-500" placeholder="Your last name..." />
+            </div>          
+        </div>
+        {/* Submit */}
+        <button className="place-self-center w-1/4 py-2 px-6 mt-4 mb-10 text-white bg-indigo-600 rounded-lg hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Submit</button>
+        </form>
+    )
+  }
+  
