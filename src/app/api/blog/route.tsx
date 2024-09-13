6aef4d036a08c7c24e20136ff404b7d813c4a69f
Weigand, Shawn <sweigand@estee.com>
2024-09-13 08:34:10 -0400

    insert to db

--- src/app/api/blog/route.tsx
+++ src/app/api/blog/route.tsx
@@ -1,6 +1,34 @@
+import pool from '../../../lib/db';
+
 export async function POST(request: Request) {
-    return new Response(JSON.stringify({ message: 'Blog created' }), {
+    const { title, content, first, last } = await request.json();
+    console.log('Request body: ', { title, content, first, last });
+    let response = {
         status: 200,
+        body: {},
+    }
+
+    if (!title || !content || !first || !last) {
+      response.status = 400
+      response.body = { message: 'Title, content, first, and last are required.' }  
+    }
+
+    try {
+        const [result] = await pool.query(
+          'INSERT INTO blogs (title, content, first_name, last_name) VALUES (?, ?, ?, ?)',
+          [title, content, first, last]
+        );
+        console.log('Result: ', result);
+        response.status = 201
+        response.body = { id: result.insertId, title, content, first, last };
+      } catch (error) {
+        console.error(error);
+        response.status = 500
+        response.body = { message: 'Database error' }
+    }
+
+    return new Response(JSON.stringify(response.body), {
+        status: response.status,
         headers: { 'Content-Type': 'application/json' },
     });
 }
