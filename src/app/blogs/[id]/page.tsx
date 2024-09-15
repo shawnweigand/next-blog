d7e5c623c6dc6a1ddc1b58a4e5845ad7ed6792e1
Weigand, Shawn <sweigand@estee.com>
2024-09-15 11:46:49 -0400

    blog page

--- src/app/blogs/[id]/page.tsx
+++ src/app/blogs/[id]/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { use, useEffect, useState } from "react";
 import Loading from "../../components/loading";
+import Link from "next/link";
 
 export default function Blog({ params }: { params: { id: string } }) {
     
@@ -17,13 +18,43 @@ export default function Blog({ params }: { params: { id: string } }) {
         fetchBlog(params.id)
     }, [])
 
+    function formatDate(timestamp: string) {
+        const date = new Date(timestamp)
+
+        const dateOptions = {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+        }
+
+        const timeOptions = {
+            hour: 'numeric',
+            minute: 'numeric',
+        }
+        
+        const formattedDate = date.toLocaleDateString('en-US', dateOptions);
+        const formattedTime = date.toLocaleTimeString('en-US', timeOptions);
+
+        return { date: formattedDate, time: formattedTime }
+    }
+
     console.log('Blog: ', blog)
 
     return (
-        <div>
-        <h1>MyBlog {blog.title}</h1>
-        <p>{blog.content}</p>
-        {blog.title ? <div className="text-white"> Hello </div> : <Loading />}
+        <div className="flex items-center justify-center h-screen bg-white">
+            {!blog.title ? 
+                
+                <Loading /> :
+                
+                <div className="grid place-items-center m-48 space-y-4">
+                    <h1 className="text-indigo-600 text-3xl text-center">{blog.title}</h1>
+                    <p className="text-gray-500 text-center">By {blog.first_name} {blog.last_name}</p>
+                    <p className="text-gray-500 text-center italic">{formatDate(blog.createdAt).date} at {formatDate(blog.createdAt).time}</p>  
+                    <p className="text-black py-16">{blog.content}</p>
+                    <Link href={'/'} className="w-32 py-2 px-6 text-white text-center bg-indigo-600 rounded-lg hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Back</Link>
+                    <Link href={'/'} className="w-32 py-2 px-6 text-white text-center bg-red-600 rounded-lg hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2">Delete</Link>
+                </div>
+            }
         </div>
     )
 }
