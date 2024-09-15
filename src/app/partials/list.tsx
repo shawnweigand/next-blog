7ffad70f3b1240cdc425438e39686a79e8c6b3df
Weigand, Shawn <sweigand@estee.com>
2024-09-15 12:13:34 -0400

    imporve list spacing

--- src/app/partials/list.tsx
+++ src/app/partials/list.tsx
@@ -28,7 +28,7 @@ export default function List({ blogs }: { blogs: Array<any> }) {
         <div className="mt-24">
             <div className="text-black">
                 {blogs.map((blog: any, index: number) => (
-                    <div key={index} className="grid grid-cols-4">
+                    <div key={index} className="grid grid-cols-6">
                         <div className="text-black text-right col-start-1 col-span-1 mb-24 mt-4">
                             <h1>{formatDate(blog.createdAt).date}</h1>
                             <h1>{formatDate(blog.createdAt).time}</h1>
@@ -37,7 +37,7 @@ export default function List({ blogs }: { blogs: Array<any> }) {
                             <div className="w-5 h-5 bg-indigo-600 rounded-full"></div>
                             {index < blogs.length - 1 && <div className="ml-2 h-full w-1 bg-indigo-300"></div>}
                         </div>
-                        <Link href={`/blogs/${encodeURIComponent(blog.id)}`} className="col-start-3 col-span-2 rounded mb-24 p-4 hover:bg-gray-100 hover:rounded-lg">
+                        <Link href={`/blogs/${encodeURIComponent(blog.id)}`} className="col-start-3 col-span-4 rounded mb-24 p-4 hover:bg-gray-100 hover:rounded-lg">
                             <h2 className="text-lg font-bold pb-2">{blog.title}</h2>
                             <p className="pb-2">{blog.content.substring(0,255)}...</p>
                             <p className="text-indigo-600 pb-2">{'Read More >'}</p>
